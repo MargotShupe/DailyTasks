8bae143e91e86b599750c27ed01b3ab171001a00
margot_shupe <jmargotromov@gmail.com>
2023-05-18 23:16:17 -0400

    CRUD added

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/react-todo", {
+  .connect("mongodb://127.0.0.1:27017/react-daily", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
@@ -17,6 +17,42 @@ mongoose
 //Models
 
 const Daily = require("./models/Daily");
+
+//testing with postman Body raw
+app.get("/dailyt", async (req, res) => {
+  const dailyt = await Daily.find();
+
+  res.json(dailyt);
+});
+
+//JSON text in postman
+app.post("/dailyt/new", (req, res) => {
+  const dailyt = new Daily({
+    text: req.body.text,
+  });
+
+  dailyt.save();
+
+  res.json(dailyt);
+});
+
+//testing by adding the id number in the localhost
+app.delete("/dailyt/delete/:id", async (req, res) => {
+  const result = await Daily.findByIdAndDelete(req.params.id);
+
+  res.json({ result });
+});
+
+//testing by adding the id number in the localhost
+app.get("/dailyt/complete/:id", async (req, res) => {
+  const dailyt = await Daily.findById(req.params.id);
+
+  dailyt.complete = !dailyt.complete;
+
+  dailyt.save();
+
+  res.json(dailyt);
+});
 
 app.listen(3000, () => {
   console.log("Backend server is running! Hola");
