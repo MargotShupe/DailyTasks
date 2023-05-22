204b54f707f4c76baa07bc93642a6efab5b966b2
margot_shupe <jmargotromov@gmail.com>
2023-05-22 18:15:12 -0400

    updating connections with the backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
-const API_BASE = "http://localhost:3001";
+const API_BASE = "https://mern-deploy1-backend.onrender.com";
+//after deploying your backend you have to now use the link from render
+//instead of "http://localhost:3001"
 
 function App() {
   const [dailyt, setDailyt] = useState([]);
